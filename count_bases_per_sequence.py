1ac4349a00fd54dbb265d15245b9d51161e45fe0
Mike Lee <leemd@usc.edu>
2017-11-01 17:00:50 -0700

    Update count_bases_per_sequence.py

--- count_bases_per_sequence.py
+++ count_bases_per_sequence.py
@@ -1,4 +1,4 @@
-### Script that spits out a txt file of one column of the number of bases in each sequence of a fasta file ###
+### Script that spits out a tab-delimited file with two columns: header, and the number of bases, for each sequence of a fasta file ###
 ### Useful for instance if calculating TPM for transcriptomic data (also, if so, read this post: http://www.rna-seqblog.com/rpkm-fpkm-and-tpm-clearly-explained/ ###
 
 from Bio import SeqIO
@@ -15,7 +15,7 @@ in_fasta = open(args.input_fasta, "r")
 out_file = open(args.output_file, "w")
 
 for seq_record in SeqIO.parse(in_fasta, "fasta"):
-  out_file.write(str(len(seq_record.seq)) + "\n")
+  out_file.write(seq_record.id + "\t" + str(len(seq_record.seq)) + "\n")
   
 in_fasta.close()
 out_file.close()
