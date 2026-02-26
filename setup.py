8220e67423dd5d5ab1ea8d7ca64f78e46f31ffc3
Mike Lee <Michael.Lee@st.dhs.gov>
2026-02-26 16:48:12 -0500

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup
 import glob
 
-# scripts ported to pyproject.toml [project.scripts] entry points
-# exclude them from the glob so they don't conflict
-ported = {
-    "bit/scripts/bit-dl-ncbi-assemblies",
-}
-
 setup(
-    scripts=[s for s in glob.glob("bit/scripts/*") if s not in ported]
+    scripts=glob.glob("bit/scripts/*")
 )
